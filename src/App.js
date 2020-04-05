b08aca868ddcd40269e687c4201e7264a44785a0
akshatpandey <akshat.pandey@pramata.com>
2020-04-05 17:25:17 +0530

    Added the footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Footer from './components/Footer/Footer'
+import classes from './App.module.css';
 
 class App extends Component{
   render() {
     return (
-      <div>
-        <p className="ui label">Must Have Caffeine !!!!!!</p>
+      <div className={classes.App}>
+        <div>
+          <p className="ui label">Must Have Caffeine !!!!!!</p>
+        </div>
+        <Footer/>
       </div>
     );
   }
