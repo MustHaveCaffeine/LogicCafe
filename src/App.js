e13c53b89ca870332db00108daddfeaec0669a7b
sainiajay <saini.ajay172@gmail.com>
2020-04-11 19:36:00 +0530

    Added Routes and Problem page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Layout from './components/Layout/Layout';
 import HomePage from './components/HomePage/HomePage';
+import ProblemsPage from './components/ProblemsPage/ProblemsPage';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component{
   render() {
     return (
       <div className={classes.App}>
         <Layout>
-          {/* <p class="ui label">Must Have Caffeine !!!</p> */}
-          <HomePage />
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/problems" component={ProblemsPage} />
+            <Route path="/problems/:id" component={ProblemsPage} />
+          </Switch>
         </Layout>
       </div>
     );
---
+++ src/components/ProblemsPage/ProblemsPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import classes from './ProblemsPage.module.css';
+
+class ProblemsPage extends Component {
+    render () {
+        return (<p>
+            Hello World
+        </p>);
+    }
+}
+
+export default ProblemsPage;
