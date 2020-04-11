9c249232ce1f6293b0e4e1138d23310d949b0c35
akshatpandey <akshat.pandey@pramata.com>
2020-04-11 20:09:48 +0530

    Minor Changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import classes from './Footer.module.css';
 import { Menu , Divider } from 'semantic-ui-react';
+import WithClass from '../../hoc/WithClass';
 
 const footer = (props) => {
     return (
-        <div className={classes.Margin}>
+        <WithClass classes={classes.Margin}>
             <Divider />
-                <div className={classes.FooterWrapper}>
-                    <Menu text>
-                        <Menu.Item header>Copyright © 2020 LogiCafe</Menu.Item>
-                        <Menu.Menu position='right'>
-                            <Menu.Item>About Us</Menu.Item>
-                            <Menu.Item>Contact</Menu.Item>
-                            <Menu.Item>Help</Menu.Item>
-                        </Menu.Menu>
-                    </Menu>
-                </div>
-        </div>
+            <div className={classes.FooterWrapper}>
+                <Menu text>
+                    <Menu.Item header>Copyright © 2020 LogiCafe</Menu.Item>
+                    <Menu.Menu position='right'>
+                        <Menu.Item>About Us</Menu.Item>
+                        <Menu.Item>Contact</Menu.Item>
+                        <Menu.Item>Help</Menu.Item>
+                    </Menu.Menu>
+                </Menu>
+            </div>
+        </WithClass>
     );
 };
 
