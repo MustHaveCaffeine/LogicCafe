b6b231a7803f31da17ae40f07cefdf10cdcdb949
sainiajay <saini.ajay172@gmail.com>
2020-04-19 18:24:11 +0530

    Some good changes

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -1,44 +1,41 @@
-import React, { Component } from 'react';
-import MonacoEditor from 'react-monaco-editor';
+import React, { Component } from "react";
+import MonacoEditor from "react-monaco-editor";
 
-const snippet = 
-`class Solution {
+const snippet = `class Solution {
     public List<String> letterCombinations(String digits) {
             
     }
 }`;
 
 class CodeEditor extends Component {
-
     state = {
-        code : snippet
-    }
+        code: snippet,
+    };
 
     editorDidMount(editor, monaco) {
         editor.focus();
     }
 
     onChange(newValue, e) {
-        console.log('onChange', newValue, e);
+        console.log("onChange", newValue, e);
     }
 
     render() {
-
         const options = {
             selectOnLineNumbers: true,
         };
-    
+
         return (
-          <MonacoEditor
-            width="800"
-            height="500"
-            language="javascript"
-            theme="vs-dark"
-            value={this.state.code}
-            options={options}
-            onChange={this.onChange}
-            editorDidMount={this.editorDidMount}
-          />
+            <MonacoEditor
+                width='800'
+                height='500'
+                language='javascript'
+                theme='vs-dark'
+                value={this.state.code}
+                options={options}
+                onChange={this.onChange}
+                editorDidMount={this.editorDidMount}
+            />
         );
     }
 }
