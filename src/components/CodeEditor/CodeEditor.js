b1d1a5e944a8ac77d2015af453b4bfd07b3b8dc3
sainiajay <saini.ajay172@gmail.com>
2020-04-20 02:28:37 +0530

    Add Ace editor + Problem provider

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -1,41 +1,63 @@
-import React, { Component } from "react";
-import MonacoEditor from "react-monaco-editor";
+import React, { Component, Fragment } from "react";
+import { Dropdown } from "semantic-ui-react";
+import AceEditor from "react-ace";
 
-const snippet = `class Solution {
-    public List<String> letterCombinations(String digits) {
-            
-    }
-}`;
+const languages = [
+    { key: 1, text: "Java", value: "java" },
+    { key: 2, text: "Python", value: "python" },
+    { key: 3, text: "Javascript", value: "javascript" },
+    { key: 4, text: "Golang", value: "golang" },
+    { key: 5, text: "Ruby", value: "ruby" },
+];
+
+const themes = ["xcode"];
+
+languages
+    .map((language) => language.value)
+    .forEach((lang) => {
+        require(`ace-builds/src-noconflict/mode-${lang}`);
+        require(`ace-builds/src-noconflict/snippets/${lang}`);
+    });
+
+themes.forEach((theme) => require(`ace-builds/src-noconflict/theme-${theme}`));
 
 class CodeEditor extends Component {
     state = {
-        code: snippet,
+        selectedLanguage: "java",
     };
 
-    editorDidMount(editor, monaco) {
-        editor.focus();
-    }
-
-    onChange(newValue, e) {
-        console.log("onChange", newValue, e);
-    }
+    changeLanguage = (_, data) => {
+        this.setState({ selectedLanguage: data.value });
+    };
 
     render() {
-        const options = {
-            selectOnLineNumbers: true,
+        const editorProps = {
+            width: "100%",
+            theme: "xcode",
+            mode: this.props.language,
+            onChange: this.props.onChange,
+            editorProps: { $blockScrolling: true },
+            setOptions: {
+                useWorker: false,
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 4,
+            },
         };
 
         return (
-            <MonacoEditor
-                width='800'
-                height='500'
-                language='javascript'
-                theme='vs-dark'
-                value={this.state.code}
-                options={options}
-                onChange={this.onChange}
-                editorDidMount={this.editorDidMount}
-            />
+            <Fragment>
+                <Dropdown
+                    search
+                    selection
+                    options={languages}
+                    onChange={this.changeLanguage}
+                    value={this.state.selectedLanguage}
+                />
+                <AceEditor {...editorProps} />
+            </Fragment>
         );
     }
 }
