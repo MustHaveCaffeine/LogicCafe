27e0e330626f9ee43122965cca1a3b396f4d3389
akshatpandey <akshat.pandey@pramata.com>
2020-04-22 15:41:15 +0530

    Adding dropdown options to tags

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { Form, Dropdown } from "semantic-ui-react";
 import WithClass from "../../../hoc/WithClass";
-import classes from './Input.module.css';
+import classes from "./Input.module.css";
 
 const input = (props) => {
     let inputElement = null;
     let inputClasses = [];
 
-    if(props.touched && props.shouldValidate && props.invalid) {
+    if (props.touched && props.shouldValidate && props.invalid) {
         inputClasses.push(classes.Invalid);
     }
 
     switch (props.elementType) {
         case "input":
-            inputElement = <input {...props.elementConfig} onChange={props.changed}/>;
+            inputElement = (
+                <input {...props.elementConfig} onChange={props.changed} />
+            );
             break;
         case "textarea":
-            inputElement = <Form.TextArea {...props.elementConfig} onChange={props.changed}/>;
+            inputElement = (
+                <Form.TextArea
+                    {...props.elementConfig}
+                    onChange={props.changed}
+                />
+            );
             break;
         case "dropdown":
             inputElement = (
@@ -30,8 +37,23 @@ const input = (props) => {
                 />
             );
             break;
+        case "multiple_dropdown":
+            inputElement = (
+                <Dropdown
+                    fluid
+                    options={props.elementConfig.options}
+                    placeholder={props.elementConfig.placeholder}
+                    onChange={props.changedTags}
+                    selection
+                    multiple
+                    value={props.value}
+                />
+            );
+            break;
         default:
-            inputElement = <input {...props.elementConfig} onChange={props.changed}/>;
+            inputElement = (
+                <input {...props.elementConfig} onChange={props.changed} />
+            );
             break;
     }
 
