55e985c68e0b6bb76a561ec0c94b86cd1059f163
akshatpandey <imakshat147@gmail.com>
2020-05-24 17:59:14 +0530

    layout component condition to render landing page

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,18 +1,21 @@
-import React, { Fragment } from 'react';
-import classes from './Layout.module.css';
-import Footer from '../../components/Footer/Footer'
-import Toolbar from '../../components/Toolbar/Toolbar'
+import React, { Fragment } from "react";
+import classes from "./Layout.module.css";
+import Footer from "../../components/Footer/Footer";
+import Toolbar from "../../components/Toolbar/Toolbar";
 
 const layout = (props) => {
-    return (
+    let content = (
         <Fragment>
-            <Toolbar/>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-            <Footer/>
+            <Toolbar />
+            <main className={classes.Content}>{props.children}</main>
+            <Footer />
         </Fragment>
     );
-}
+    if (window.location.pathname === "/") {
+        content = <Fragment>{props.children}</Fragment>;
+    }
+
+    return content;
+};
 
 export default layout;
